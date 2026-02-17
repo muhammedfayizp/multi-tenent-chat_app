492581fac3a007b80adda489f1874335bc9d7941
muhammed fayiz <fayizp6235@gmail.com>
2026-02-17 13:02:23 +0530

    server config file updated

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     port: parseInt(process.env.PORT || '4000', 10),
-    mongoURL: process.env.mongoURL || 'mongodb://localhost:27017/Chatapp',
+    mongoURL: process.env.MONGODB_URL|| 'mongodb://localhost:27017/Chatapp',
 
    
 };
